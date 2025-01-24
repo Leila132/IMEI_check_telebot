ee116394e9b3c624b1ad15fabb5265c17473bec9
Leila Saryimova <saryymova@mail.ru>
2025-01-24 15:10:17 +0300

    update whitelist

--- whitelist.py
+++ whitelist.py
@@ -3,5 +3,9 @@ from database import get_db_connection, create_tables, insert_data
 conn = get_db_connection()
 create_tables(conn)
 
-insert_data(conn, "1")
+with open('whitelist.txt', 'r') as file:
+    for line in file:
+        user_id = line.strip() 
+        insert_data(conn, user_id)
+
 conn.close()
